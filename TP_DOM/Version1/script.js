d049456c359b54b007a61c61b05bd43ec25f9be4
Xelaanyle <Alechemin@gmail.com>
2023-08-20 17:32:56 +0200

    Version améliorer

--- TP_DOM/Version1/script.js
+++ TP_DOM/Version1/script.js
@@ -1,14 +1,7 @@
 const boutonToggle = document.getElementById("bouton-toggle");
 const tableauNote = document.getElementById("tableau-note");
 const inputs = tableauNote.querySelectorAll("input");
-let modeCouleur = false;
-
-boutonToggle.addEventListener("click", () => {
-    modeCouleur = !modeCouleur;
-    console.log("mode couleur : " + modeCouleur)
-    boutonToggle.textContent = modeCouleur ? "Mode couleur" : "Mode note";
-    reinitialiserSaisies();
-});
+let modeCouleur = true;
 
 function couleurValeur(valeur) {
     switch (valeur) {
@@ -18,7 +11,7 @@ function couleurValeur(valeur) {
         case "4": return "green";
         default: return "green";
     }
-}
+};
 
 function reinitialiserSaisies() {
     inputs.forEach(input => {
@@ -30,12 +23,30 @@ function reinitialiserSaisies() {
         }
         input.removeAttribute("hidden"); 
     });
-}
+};
+
+function champSuivant(input) {
+    const colonneParent = input.parentElement;
+    const ligneSuivante = colonneParent.parentElement.nextElementSibling;
+    if (ligneSuivante) {
+        const prochaineSaisie = ligneSuivante.querySelector("input");
+        if (prochaineSaisie) {
+            prochaineSaisie.focus();
+        }
+    }
+};
+
+boutonToggle.addEventListener("click", () => {
+    modeCouleur = !modeCouleur;
+    console.log("mode couleur : " + modeCouleur)
+    boutonToggle.textContent = modeCouleur ? "Mode couleur" : "Mode note";
+    reinitialiserSaisies();
+});
 
 inputs.forEach(input => {
     input.addEventListener("input", function () {
         const valeurActuelle = this.value;
-        
+
         if (modeCouleur) {
             const valideCouleur = /^[1-4]$/.test(valeurActuelle);
             if (!valideCouleur) {
@@ -43,17 +54,9 @@ inputs.forEach(input => {
                 input.value = "";
                 return;
             }
-            
-            input.style.backgroundColor = couleurValeur(valeurActuelle);
 
-            const celluleParente = input.parentElement;
-            const ligneSuivante = celluleParente.parentElement.nextElementSibling;
-            if (ligneSuivante) {
-                const prochaineSaisie = ligneSuivante.querySelector("input");
-                if (prochaineSaisie) {
-                    prochaineSaisie.focus();
-                }
-            }
+            input.style.backgroundColor = couleurValeur(valeurActuelle);
+            champSuivant(input);
         } else {
             const valideNote = /^[1-5]$/.test(valeurActuelle);
             if (!valideNote) {
@@ -61,25 +64,17 @@ inputs.forEach(input => {
                 input.value = "";
                 return;
             }
-            
-            const colonneParente = input.parentElement;
-            const ligneSuivante = colonneParente.parentElement.nextElementSibling;
-            if (ligneSuivante) {
-                const prochaineSaisie = ligneSuivante.querySelector("input");
-                if (prochaineSaisie) {
-                    prochaineSaisie.focus();
-                }
-            }
+
+            champSuivant(input);
         }
         setTimeout(() => {
             input.blur();
-        },1000)
+        }, 1000);
     });
-    
+
     input.addEventListener("blur", () => {
         if (modeCouleur) {
             input.value = "";
         }
     });
 });
-
